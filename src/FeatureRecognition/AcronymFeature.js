f554126e24df8fa03b7719e1922fd8e4a9bed2f3
Kévin Masseix <kmasseix@fashionnetwork.com>
2019-06-04 02:37:26 +0200

    Acronyms are no numbers and contains at least two chars

--- src/FeatureRecognition/AcronymFeature.js
+++ src/FeatureRecognition/AcronymFeature.js
@@ -6,6 +6,8 @@ export default class AcronymFeature {
             .replace(/[^a-zA-Z 0-9]+/g," ")
             .split(' ')
             .filter(value => value.toLocaleUpperCase() === value)
+            .filter(value => value.length > 1)
+            .filter(value => Number.isNaN(Number(value)))
         ;
     }
 }
